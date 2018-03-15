11fb640394a61cdecc5b56c41bc0efa334abeb92
Kyle San Clemente <kylesanclemente@gmail.com>
2018-03-14 17:27:15 -0700

    created function to calculate the change combinations for an amount of money

--- lib/searches.ex
+++ lib/searches.ex
@@ -27,4 +27,63 @@ defmodule Algorithms.Searches do
   @spec seq_search_max([integer], integer) :: integer
   defp seq_search_max([head | tail], max), do: seq_search_max(tail, Kernel.max(max, head))
   defp seq_search_max([], max), do: max
+
+  @doc """
+  calculates combinations of change
+  ### Algorithm Analysis
+    * Lower Bounds:
+    * Complexity: O(n)
+
+  ## Examples
+    iex> Algorithms.Searches.change(17)
+    6
+
+    iex> Algorithms.Searches.change(50)
+    50
+
+    iex> Algorithms.Searches.change(51)
+    50
+
+    iex> Algorithms.Searches.change(25)
+    13
+
+    iex> Algorithms.Searches.change(10)
+    4
+
+  """
+  @spec change(integer) :: integer
+  def change(amount), do: half_dollar(amount)
+
+  defp half_dollar(amount) do
+    if amount >= 50 do
+      quarter(amount) + half_dollar(amount - 50)
+    else
+      quarter(amount)
+    end
+  end
+
+  defp quarter(amount) do
+    if amount >= 25 do
+     dime(amount) + quarter(amount - 25)
+    else
+      dime(amount)
+    end
+  end
+
+  defp dime(amount) do
+    if amount >= 10 do
+     nickel(amount) + dime(amount - 10)
+    else
+      nickel(amount)
+    end
+  end
+
+  defp nickel(amount) do
+    if amount >= 0 do
+      div(amount, 5) + 1
+    else
+      0
+    end
+  end
+
 end

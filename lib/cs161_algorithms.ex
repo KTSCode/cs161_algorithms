ce8fa9620ac43873b4f13ab203ba007b3fdc3163
Kyle San Clemente <kylesanclemente@gmail.com>
2018-03-17 14:16:52 -0700

    Added to each section of final study guide

--- lib/cs161_algorithms.ex
+++ lib/cs161_algorithms.ex
@@ -1,7 +1,8 @@
 defmodule Algorithms do
   @moduledoc """
-  # CS 161 Algorithms:
-  ## Algorithms
+  # CS 161 Algorithms Final Study Guide:
+  ## Course Intro
+  ### Algorithms
     * **Algorithm:** Rules which give a sequence of operations for solving a problem
       * Characteristics:
         * *Finite* - must terminate after a finite number of steps
@@ -9,20 +10,20 @@ defmodule Algorithms do
         * *Effective* - each step must be doable in finite time
         * *Produces output* and might have *input*
 
-  ## Complexity and Asymptotics
+  ### Complexity and Asymptotics
     * **O(f)** = {g : N → R∗ | ∃c ∈ R+, ∃n0 ∈ N, ∀n ≥ n0 g(n) ≤ c · f(n)}
       * we refer to O(f) as *order of f* or *big-O of f*
       * we often say g is O(f), but we mean that g is in O(f)
       * we abuse the notation by allowing a g(n) to be in the order of f even when each of f and f have a finite number of negative values
     * **Ω(f)** = {g : N → R∗ | ∃c ∈ R+, ∃n0 ∈ N, ∀n ≥ n0 g(n) ≥ c · f(n)}
     * **θ(f)** = Ω(f) ∩ O(f)
-  ### for functions in N → R∗
+  #### for functions in N → R∗
     * limit n→∞ g(n)/f(n) = C > 0 ⇒ g ∈ θ(f)
     * limit n→∞ g(n)/f(n) = C >= 0 ⇒ g ∈ O(f)
     * limit n→∞ g(n)/f(n) = C = 0 ⇒ g ∈ o(f)
     * limit n→∞ g(n)/f(n) = ∞ ⇒ g ∈ Ω(f)
 
-  ## Models of Computation
+  ### Models of Computation
     * **Arithmetic Model** - when arithmetic operations dominate cost
       * variables can hold any integer
       * each variable takes 1 unit of space
@@ -43,6 +44,135 @@ defmodule Algorithms do
         * Any mathematical operation can be done in no time
         * Assignments, I/O, branching take no time
         * Comparisons take 1 unit of time
+
+   ## Searching
+   ### Sequential Seach
+     * Ordered list
+
+   ### Binary Seach
+     * Ordered list
+
+   ### Interpolation Seach
+     * "Looking up cat in a dictionary you expect to find it at the beginning"
+
+   ## Sorting and Lower Bounds
+   ### Insertion Sort
+     * Binary insertion sort: uses binary search to find correct place
+     * Shellsort
+
+   ### Lower Bounds on Sorting
+     * **Inversion** - an inversion within a seqence of values is a pair of values that er out of order relative to eachother
+
+   ### Heapsort
+     * Maxheap
+
+   ### Distribution Sort
+     * Bucket Sort
+     * Radix Sort
+
+   ### Lower Bounds on Selection
+     * MaxMin
+     * Second Largest
+     * Median
+
+   ## Divide and Conquer
+     * Basic Idea
+     * Three Steps
+     * Analysis
+   ### Selection of Median
+     * Selection
+     * QuickSelect
+       * Analysis of QuickSelect
+     * DSelect
+       * Analysis
+     * Weighted Median
+
+   ### MaxMin
+     * Example
+
+   ### Integer Multiplication
+     * Example
+     * Karatsuba
+
+   ### Divide and Conquer Theorem
+     * Time Complexity
+     * Theorem:
+     * Unrolling
+     * Expanding
+     * General
+   #### The Master Theorem
+     * Theorem
+
+   ### Matrix Multiplication
+     * Strassen
+     * The Skyline Problem
+
+
+   ## Dynamic Programming
+   ### Introduction
+     * **Dynamic Programming** - can be thought of as reverse recursion
+
+   ### Product of Matrices
+     * Recursion vs DP
+
+   ### Common Substrings and Subsequences
+     * **Problem** - Given 2 sequences, `A[1:m]` and `B[1:n]`, `m >= n` find the longest contiguous sequence appearing in both
+
+   ### Optimal Binary Search Trees
+     * Values of IPL in BST
+     * Avg Value of IPL in BST
+     * Avg Cost to Query a BST
+     * Solution Approach
+     * Subproblems
+     * Recurrence
+
+   ### Nim
+     * Game Description
+     * Algorithm DP
+     * Algorithm Recursion with Memoization
+
+   ### Fibonacci Numbers
+     * Definition
+     * Recursion
+     * DP
+
+   ## Graph Algorithms
+   ### Minimum Spanning Trees
+     * Algorithms
+      * Prim's
+      * Kruskal's
+      * Boruvka's
+      * Hybrid
+
+   ### Depth-first Search
+     * Example
+
+   ### Biconnected Components
+     * Articulation Point
+
+   ### Transitive Closure
+     * reflexive
+     * irreflexive
+
+   ### Shortest Path
+     * Floyd-Warshall
+     * All-pairs
+     * Dijkstra's
+
+
+   ## Other Topics
+   ### Pseudo-Polynomial Algorithms
+   #### Knapsack Problem
+     * Recurrence
+     * Dynamic Programming
+     * Integer KP
+
+   ### KMP String Matching
+     * **Problem** - Find where a pattern occurs within a text
+
+   ### Probabilistic Search Algorithm
+     * Searching an ordered linked list in arrays
+
 
   """
 end
